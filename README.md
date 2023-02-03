6f878f3f6e27a27bf3dceb7807f3e7f860167687
batman <vlassov@kolesa.kz>
2023-02-03 09:16:14 +0600

    some enhancing

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # prometheus_postgresql_exporter
+Usage:
+git clone
+cargo build --release
+./target/release/prometheus-postgresql-exporter my-postgresql-ho my-database my-user my-password
+curl localhost:8080/metrics
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
 use axum::Router;
-use prometheus::{Counter, Encoder, Gauge, Histogram, Registry, TextEncoder};
 use rust_decimal::prelude::*;
-use rust_decimal_macros::dec;
 use std::net::SocketAddr;
-use std::ops::Add;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use tokio_postgres::{Error, NoTls};
+use tokio_postgres::NoTls;
+use clap::{Command, Arg};
+use lazy_static::lazy_static;
 
-struct Metrics {
-    connection_errors: Counter,
-    seq_scan: Counter,
-    seq_tup_read: Counter,
-    idx_scan: Counter,
-    avg: Counter,
+lazy_static! {
+    static ref ARGS: clap::ArgMatches = Command::new("Postgresql exporter")
+        .version("1.0")
+        .author("batman")
+        .arg(
+            Arg::new("host")
+                .help("Postgresql host")
+                .required(true),
+        )
+        .arg(
+            Arg::new("database")
+                .help("Postgresql database")
+                .required(true),
+        )
+        .arg(
+            Arg::new("user")
+                .help("Postgresql user")
+                .required(true),
+        )
+        .arg(
+            Arg::new("password")
+                .help("Postgresql password")
+                .required(true),
+        )
+    .get_matches();
 }
 
 #[derive(Debug)]
-struct Common_effectiveness {
+struct CommonEffectiveness {
     relname: String,
     seq_scan: i64,
     seq_tup_read: i64,
@@ -30,59 +46,27 @@ struct Common_effectiveness {
 }
 
 #[derive(Debug)]
-struct Index_usage {
+struct IndexUsage {
     relname: String,
     percent_of_times_index_used: i64,
     rows_in_table: i64,
 }
 
 #[derive(Debug)]
-struct Hit_miss {
+struct HitMiss {
     heap_read: Decimal,
     heap_hit: Decimal,
     ratio: Decimal,
 }
 
-impl Metrics {
-    fn new() -> Self {
-        let registry = Registry::new();
-        let seq_scan = Counter::new(
-            "seq_scan",
-            "Количество последовательных чтений, запущенных по этой таблице",
-        )
-        .unwrap();
-        registry.register(Box::new(seq_scan.clone())).unwrap();
-        let seq_tup_read = Counter::new(
-            "seq_tup_read",
-            "Количество 'живых' строк, прочитанных при последовательных чтениях",
-        )
-        .unwrap();
-        registry.register(Box::new(seq_tup_read.clone())).unwrap();
-        let idx_scan = Counter::new(
-            "idx_scan",
-            "Количество запущенных сканирований по этому индексу",
-        )
-        .unwrap();
-        registry.register(Box::new(idx_scan.clone())).unwrap();
-        let avg = Counter::new("avg", "Отношение seq_tup_read к seq_scan").unwrap();
-        registry.register(Box::new(avg.clone())).unwrap();
-        let connection_errors = Counter::new("connection_errors", "Connection errors").unwrap();
-        registry
-            .register(Box::new(connection_errors.clone()))
-            .unwrap();
-        Self {
-            seq_scan,
-            seq_tup_read,
-            idx_scan,
-            avg,
-            connection_errors,
-        }
-    }
-}
-
 async fn metrics() -> String {
     let (postgres_client, connection) = tokio_postgres::connect(
-        "host=localhost user=123 password=123 dbname=123",
+        format!("host={} user={} password={} dbname={}",
+                ARGS.get_one::<String>("host").unwrap(),
+                ARGS.get_one::<String>("user").unwrap(),
+                ARGS.get_one::<String>("password").unwrap(),
+                ARGS.get_one::<String>("database").unwrap())
+            .as_str(),
         NoTls,
     )
     .await
@@ -92,14 +76,32 @@ async fn metrics() -> String {
             eprintln!("connection error: {}", e);
         }
     });
-    let common_effectiveness_statement = postgres_client.prepare("SELECT relname, seq_scan, seq_tup_read, idx_scan, vacuum_count, autovacuum_count, analyze_count, autoanalyze_count, seq_tup_read/seq_scan as avg FROM pg_stat_user_tables WHERE seq_scan > 0 ORDER BY seq_tup_read DESC").await.unwrap();
+    let common_effectiveness_statement = postgres_client
+        .prepare(
+            "SELECT
+                relname,
+                seq_scan,
+                seq_tup_read,
+                idx_scan,
+                vacuum_count,
+                autovacuum_count,
+                analyze_count,
+                autoanalyze_count,
+                seq_tup_read/seq_scan as avg
+            FROM
+                pg_stat_user_tables
+            WHERE
+                seq_scan > 0
+            ORDER BY seq_tup_read DESC")
+        .await
+        .unwrap();
     let common_effectiveness_rows = postgres_client
         .query(&common_effectiveness_statement, &[])
         .await
         .unwrap();
-    let common_effectiveness: Vec<Common_effectiveness> = common_effectiveness_rows
+    let common_effectiveness: Vec<CommonEffectiveness> = common_effectiveness_rows
         .iter()
-        .map(|row| Common_effectiveness {
+        .map(|row| CommonEffectiveness {
             relname: row.get(0),
             seq_scan: row.get(1),
             seq_tup_read: row.get(2),
@@ -111,14 +113,22 @@ async fn metrics() -> String {
             avg: row.get(8),
         })
         .collect();
-    let hit_miss_statement = postgres_client.prepare("SELECT sum(heap_blks_read) as heap_read, sum(heap_blks_hit) as heap_hit, sum(heap_blks_hit) / (sum(heap_blks_hit) + sum(heap_blks_read)) as ratio FROM pg_statio_user_tables").await.unwrap();
+    let hit_miss_statement = postgres_client
+        .prepare(
+            "SELECT
+                sum(heap_blks_read) as heap_read,
+                sum(heap_blks_hit) as heap_hit,
+                sum(heap_blks_hit) / (sum(heap_blks_hit) + sum(heap_blks_read)) as ratio
+            FROM pg_statio_user_tables")
+        .await
+        .unwrap();
     let hit_miss_rows = postgres_client
         .query(&hit_miss_statement, &[])
         .await
         .unwrap();
-    let hit_miss: Vec<Hit_miss> = hit_miss_rows
+    let hit_miss: Vec<HitMiss> = hit_miss_rows
         .iter()
-        .map(|row| Hit_miss {
+        .map(|row| HitMiss {
             heap_read: row.get(0),
             heap_hit: row.get(1),
             ratio: row.get(2),
@@ -127,25 +137,25 @@ async fn metrics() -> String {
     let index_usage_statement = postgres_client
         .prepare(
             "SELECT
-          relname,
-          100 * idx_scan / (seq_scan + idx_scan) percent_of_times_index_used,
-          n_live_tup rows_in_table
-        FROM
-          pg_stat_user_tables
-        WHERE
-            seq_scan + idx_scan > 0
-        ORDER BY
-          n_live_tup DESC;",
-        )
-        .await
-        .unwrap();
+              relname,
+              100 * idx_scan / (seq_scan + idx_scan) percent_of_times_index_used,
+              n_live_tup rows_in_table
+            FROM
+              pg_stat_user_tables
+            WHERE
+                seq_scan + idx_scan > 0
+            ORDER BY
+              n_live_tup DESC;",
+            )
+            .await
+            .unwrap();
     let index_usage_rows = postgres_client
         .query(&index_usage_statement, &[])
         .await
         .unwrap();
-    let index_usage: Vec<Index_usage> = index_usage_rows
+    let index_usage: Vec<IndexUsage> = index_usage_rows
         .iter()
-        .map(|row| Index_usage {
+        .map(|row| IndexUsage {
             relname: row.get(0),
             percent_of_times_index_used: row.get(1),
             rows_in_table: row.get(2),
@@ -155,49 +165,49 @@ async fn metrics() -> String {
     for i in common_effectiveness {
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.seq_scan.{}: {}\n",
+                "postgresql.common_effectiveness.seq_scan.{}: {}\n",
                 i.relname, i.seq_scan
             )
             .as_str();
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.seq_tup_read.{}: {}\n",
+                "postgresql.common_effectiveness.seq_tup_read.{}: {}\n",
                 i.relname, i.seq_tup_read
             )
             .as_str();
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.idx_scan.{}: {}\n",
+                "postgresql.common_effectiveness.idx_scan.{}: {}\n",
                 i.relname, i.idx_scan
             )
             .as_str();
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.vacuum_full_count.{}: {}\n",
+                "postgresql.common_effectiveness.vacuum_full_count.{}: {}\n",
                 i.relname, i.vacuum_full_count
             )
             .as_str();
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.autovacuum_count.{}: {}\n",
+                "postgresql.common_effectiveness.autovacuum_count.{}: {}\n",
                 i.relname, i.autovacuum_count
             )
             .as_str();
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.analyze_count.{}: {}\n",
+                "postgresql.common_effectiveness.analyze_count.{}: {}\n",
                 i.relname, i.analyze_count
             )
             .as_str();
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.autoanalyze_count.{}: {}\n",
+                "postgresql.common_effectiveness.autoanalyze_count.{}: {}\n",
                 i.relname, i.autoanalyze_count
             )
             .as_str();
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.avg.{}: {}\n",
+                "postgresql.common_effectiveness.avg.{}: {}\n",
                 i.relname, i.avg
             )
             .as_str();
@@ -205,29 +215,29 @@ async fn metrics() -> String {
     for i in hit_miss {
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.heap_read: {}\n",
+                "postgresql.hit_miss.heap_read: {}\n",
                 i.heap_read
             )
             .as_str();
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.heap_hit: {}\n",
+                "postgresql.hit_miss.heap_hit: {}\n",
                 i.heap_hit
             )
             .as_str();
         result =
-            result + format!("test.postgresql.common_effectiveness.ratio: {}\n", i.ratio).as_str();
+            result + format!("postgresql.hit_miss.ratio: {}\n", i.ratio).as_str();
     }
     for i in index_usage {
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.percent_of_times_index_used.{}: {}\n",
+                "postgresql.index_usage.percent_of_times_index_used.{}: {}\n",
                 i.relname, i.percent_of_times_index_used
             )
             .as_str();
         result = result
             + format!(
-                "test.postgresql.common_effectiveness.rows_in_table.{}: {}\n",
+                "postgresql.index_usage.rows_in_table.{}: {}\n",
                 i.relname, i.rows_in_table
             )
             .as_str();
@@ -237,7 +247,7 @@ async fn metrics() -> String {
 
 #[tokio::main]
 async fn main() {
-    let server = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let server = SocketAddr::from(([0, 0, 0, 0], 8080));
     let app = Router::new().route("/metrics", axum::routing::get(metrics));
     // Start server.
     axum::Server::bind(&server)
